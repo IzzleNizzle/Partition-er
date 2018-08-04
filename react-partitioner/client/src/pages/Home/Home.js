ed880e7d8c109b0d76b36cd931c392cdf32e8843
ctrlz <pehisaac@gmail.com>
2018-08-03 22:30:31 -0600

    Trying to get Material UI installed. Not yet there.

---
+++ react-partitioner/client/src/pages/Home/Home.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import Button from '@material-ui/core/Button';
+// import Typography from '@material-ui/core/Typography';
+import API from "../../utils/API";
+
+
+
+class Home extends Component {
+  state = {
+    books: [],
+    title: "",
+    author: "",
+    synopsis: ""
+  };
+
+  // componentDidMount() {
+  //   // this.loadBooks();
+  // }
+
+  // loadBooks = () => {
+  //   API.getBooks()
+  //     .then(res =>
+  //       this.setState({ books: res.data, title: "", author: "", synopsis: "" })
+  //     )
+  //     .catch(err => console.log(err));
+  // };
+
+  // deleteBook = id => {
+  //   API.deleteBook(id)
+  //     .then(res => this.loadBooks())
+  //     .catch(err => console.log(err));
+  // };
+
+  // handleInputChange = event => {
+  //   const { name, value } = event.target;
+  //   this.setState({
+  //     [name]: value
+  //   });
+  // };
+
+  // handleFormSubmit = event => {
+  //   event.preventDefault();
+  //   if (this.state.title && this.state.author) {
+  //     API.saveBook({
+  //       title: this.state.title,
+  //       author: this.state.author,
+  //       synopsis: this.state.synopsis
+  //     })
+  //       .then(res => this.loadBooks())
+  //       .catch(err => console.log(err));
+  //   }
+  // };
+
+  render() {
+    return (
+      <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+    );
+  }
+}
+
+export default Home;
