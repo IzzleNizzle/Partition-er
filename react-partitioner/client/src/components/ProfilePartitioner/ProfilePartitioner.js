e3f20f7ac587cf8811439fae80c4468dcffe26fc
ctrlz <pehisaac@gmail.com>
2018-08-18 19:00:29 -0600

    Created and tested linked files for first component prototype.

---
+++ react-partitioner/client/src/components/ProfilePartitioner/ProfilePartitioner.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./ProfilePartitioner.css"
+
+
+
+class ProfilePartitioner extends Component {
+
+  render() {
+    return (
+      <div className="profile-partitioner"></div>
+    );
+  }
+}
+
+export default ProfilePartitioner;
--- react-partitioner/client/src/pages/Home/Home.js
+++ react-partitioner/client/src/pages/Home/Home.js
@@ -2,59 +2,15 @@ import React, { Component } from "react";
 // import Button from '@material-ui/core/Button';
 // import Typography from '@material-ui/core/Typography';
 import API from "../../utils/API";
-
+import ProfilePartitioner from "../../components/ProfilePartitioner";
 
 
 class Home extends Component {
-  state = {
-    books: [],
-    title: "",
-    author: "",
-    synopsis: ""
-  };
 
-  // componentDidMount() {
-  // }
-
-  // loadBooks = () => {
-  //   API.getBooks()
-  //     .then(res =>
-  //       this.setState({ books: res.data, title: "", author: "", synopsis: "" })
-  //     )
-  //     .catch(err => console.log(err));
-  // };
-
-  // deleteBook = id => {
-  //   API.deleteBook(id)
-  //     .then(res => this.loadBooks())
-  //     .catch(err => console.log(err));
-  // };
-
-  // handleInputChange = event => {
-  //   const { name, value } = event.target;
-  //   this.setState({
-  //     [name]: value
-  //   });
-  // };
-
-  // handleFormSubmit = event => {
-  //   event.preventDefault();
-  //   if (this.state.title && this.state.author) {
-  //     API.saveBook({
-  //       title: this.state.title,
-  //       author: this.state.author,
-  //       synopsis: this.state.synopsis
-  //     })
-  //       .then(res => this.loadBooks())
-  //       .catch(err => console.log(err));
-  //   }
-  // };
 
   render() {
     return (
-      <button variant="contained" color="primary">
-      Hello World
-    </button>
+      <ProfilePartitioner />
     );
   }
 }
