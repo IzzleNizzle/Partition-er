55a3959cd8285843f59fa833fb509ebe4c5047da
ctrlz <pehisaac@gmail.com>
2018-08-20 13:26:04 -0600

    Updating some valuevisualizer data, getting mapped objects and such..

---
+++ react-partitioner/client/src/components/ValueVisualizer/ValueVisualizer.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+// import Button from '@material-ui/core/Button';
+// import Typography from '@material-ui/core/Typography';
+import API from "../../utils/API";
+import "./ValueVisualizer.css"
+
+
+/*
+
+  I'm thinking i could set this component up like this. once I get the data for the objec and what will be isnide of it, then i'll just send that as an object as props and i'll just interpret that here.
+
+
+  object mapping:
+
+  actually I'll have two props coming in, we'll just do the name once so we don't have to send tha tover and over again, then we'll just send an object of objects for teh value
+
+
+ I would map out these objects and just make a box for each one
+  
+  {
+
+      {
+        name: // name of the unique inpu
+        value: // how much the unique input was
+      }
+
+
+
+  }
+
+
+*/
+
+
+
+class ValueVisualizer extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      <div className="value-visualizer">
+
+        {/* Displays value of profile */}
+        <span id="display-value">$100</span>
+
+        {/* Maps out profile boxes */}
+        {
+          this.props.segments.map(
+          (segment, index) =>
+            <div key={index}>
+            {segment.value}
+            {segment.name}
+            </div>
+        )
+        }
+
+      </div>
+    );
+  }
+}
+
+export default ValueVisualizer;
--- react-partitioner/client/src/pages/Home/Home.js
+++ react-partitioner/client/src/pages/Home/Home.js
@@ -4,6 +4,53 @@ import React, { Component } from "react";
 import API from "../../utils/API";
 import ProfilePartitioner from "../../components/ProfilePartitioner";
 import Box from "../../components/Box";
+import ValueVisualizer from "../../components/ValueVisualizer"
+
+
+/*
+
+Here's a graveyard for the building pieces I believe i'll need to put this together...
+
+//used for counting table data
+var boxCount = 1;
+
+//variable to store latest value of the left property for boxes
+var leftValue = 0;
+
+// Account 1 value
+var acct1Value = 100;
+var valueTotal = 0;
+
+************************************************************
+
+Logic for calculations
+
+  // When an input is submitted, here we are getting that input number and adding it to the 'valuetotal' variable
+
+    var value = parseFloat($("#value1").val().trim());
+    // keeps tally of all the values
+    valueTotal += value;
+
+  when an input is processed, send another add another little box to the visualizer
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
 
 class Home extends Component {
   constructor(props){
@@ -14,22 +61,41 @@ class Home extends Component {
   render() {
     return (
       <ProfilePartitioner >
+
         <Box >
           <h1> This is where you would put hte name of the project
           </h1>
         </ Box>
+
         <Box >
-          <h1> Data visualization
-          </h1>
+        <ValueVisualizer 
+          segments={
+            // Sending an array of objects to the component for initial mapping
+            [
+              {
+                name: 'Here we go',
+                value: 50
+              },
+              {
+                name: 'Another test',
+                value: 50
+              }
+            ]
+
+          }
+        />
         </ Box>
+
         <Box >
           <h1> User action input
           </h1>
         </ Box>
+
         <Box >
           <h4> Data Table
           </h4>
         </ Box>
+
       </ProfilePartitioner>
     );
   }
